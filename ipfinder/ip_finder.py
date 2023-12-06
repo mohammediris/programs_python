3f8c4815d38cc28fb01dd191009834e679c75cea
Mohammed Iris <mohammedirisa@gmail.com>
2023-12-06 23:29:40 +0400

    all

--- ipfinder/ip_finder.py
+++ ipfinder/ip_finder.py
@@ -3,21 +3,33 @@ from scapy.all import ARP, sniff
 import psutil
 import socket
 import sys
+import re
 """
 Functions defined
 """
 def enable_dhcp(interface_name): 
+    
     command = f"netsh interface ipv4 set address name=\"{interface_name}\" source=dhcp"
     result = subprocess.run(command, stdout=subprocess.PIPE, shell=True)
+    main_string = str(result.stdout.decode('utf-8').strip())
+    pattern = r'\bRun as administrator\b'
+    #sys.exit()
     if result.returncode==0:
         print (f"\nDHCP enabled on interface: {interface_name}")
+    elif re.search(pattern, main_string):     
+        
+        print('\n'+result.stdout.decode('utf-8').strip())
+        input("Please right-click the file and select \"Run as Administrator\"")
+        sys.exit()
     else:
         print('\n'+result.stdout.decode('utf-8').strip())
+        
     return result.returncode
-
+    
 def arp_display(pkt):
     if ARP in pkt and pkt[ARP].op == 1:  # If it's an ARP request (who-has)
-        print(f"IP Address found: {pkt[ARP].psrc}")
+        print(f"\nA device with \nIP\t\t: {pkt[ARP].psrc}\nMAC Address\t: {pkt[ARP].hwsrc}\nis sending ARP Request")
+        
         
 def display_ipv4_interfaces():
     # Get network interface addresses
@@ -36,7 +48,7 @@ def display_ipv4_interfaces():
 Main Code
 """
 if __name__ == "__main__":
-    timeout = 60 # set timeout for sniffing if not packets received
+    timeout = 30 # set timeout for sniffing if not packets received
     ifaces_list = display_ipv4_interfaces()
     for iface in ifaces_list:
         print (str(ifaces_list.index(iface)+1) + ' ' + iface)
@@ -51,8 +63,9 @@ if __name__ == "__main__":
             print(f"\nSearching for Source IP in ARP broadcast in interface \"{interface}\" for {timeout} seconds")
             # sniffing network traffic for ARP packets
             sniff(filter="arp", prn=arp_display, store=0, iface=interface, count=5, timeout=timeout)
+            input()
         except KeyboardInterrupt:
             sys.exit(0)
     else:
         print("\nSelected Network Interface is Down, Please check the connection.")
-        input()
+        input("\nPress any key to exit.")
